75f1c960cc91583cc9b704ce306c1cab4ae8f0b2
ade22emi <adetutu@yahoo.com>
2025-03-20 17:24:16 +0100

    update2

--- push.sh
+++ push.sh
@@ -1,7 +1,7 @@
 #!bin/bash
-cd /home/adetutu/teachings/autodevops_class
+cd /home/adetutu/autodevops_class
 git status -C
 git init 
 git add .
-git commit -m "update"
+git commit -m "update2"
 git push origin master
